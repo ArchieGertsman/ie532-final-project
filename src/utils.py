02a49f8ff3c5cb8de4ad13e7f060ecf7b92e06d8
archiegertsman <arkadiy2@illinois.edu>
2022-11-21 12:56:26 -0600

    added sparse graph

--- src/utils.py
+++ src/utils.py
@@ -9,21 +9,28 @@ def euclidean(G, gate1, gate2):
     return np.sqrt((x1-x2)**2 + (y1-y2)**2) 
 
 
+def add_edge_euclidean(G, gate1, gate2):
+    G.add_edge(gate1, gate2, dist=euclidean(G, gate1, gate2))
+
+
+def add_edges_eucliean(G, gate_pairs):
+    G.add_weighted_edges_from([
+        (gate1, gate2, euclidean(G, gate1, gate2))
+        for gate1, gate2 in gate_pairs
+    ], weight='dist')
+
+
 def connect_group_in_series(G, group):
-    connect_groups_accross(G, group[:-1], group[1:])
+    add_edges_eucliean(G, zip(group[:-1], group[1:]))
 
 
 def connect_group_fully(G, group):
-    G.add_weighted_edges_from([
-        (gate1, gate2, euclidean(G, gate1, gate2))
-        for gate1, gate2 in product(group, group) if gate1 != gate2
-    ], weight='length')
+    add_edges_eucliean(G, ((gate1, gate2) 
+        for gate1, gate2 in product(group, group) 
+        if gate1 != gate2
+    ))
 
 
 def connect_groups_accross(G, group1, group2):
     assert len(group1) == len(group2)
-
-    G.add_weighted_edges_from([
-        (gate1, gate2, euclidean(G, gate1, gate2))
-        for gate1, gate2 in zip(group1, group2)
-    ], weight='length')
+    add_edges_eucliean(G, zip(group1, group2))
