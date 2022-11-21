aa824f7c0ec0b16af66af41808fb62fbf4c2c47e
archiegertsman <arkadiy2@illinois.edu>
2022-11-21 16:56:35 -0600

    started implementing flight generator

--- src/gate_graph_loader.py
+++ src/gate_graph_loader.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import pandas as pd
-import numpy as np
 import pandas as pd
 
 from .utils import *
@@ -12,6 +11,8 @@ class GateGraphLoader:
 
 
     def _setup_gate_graph(self, gate_pos_filename):
+        '''add gates to graph as nodes, reading node positions
+        from csv named `gate_pos_filename`'''
         self.G = nx.Graph() # direction does not matter for gate transport
         df = pd.read_csv(gate_pos_filename, index_col='gate')
         for terminal, (x,y) in df.iterrows():
@@ -22,22 +23,25 @@ class GateGraphLoader:
         G = self.G.copy()
 
         # concourse A
-        add_edge_euclidean(G, 'WH', 'A1')
-        add_edge_euclidean(G, 'WH', 'A4A')
+        connect_gates(G, 'WH', 'A1')
+        connect_gates(G, 'WH', 'A4A')
         connect_group_in_series(G, ['A1', 'A2', 'A3', 'A5'])
-        connect_group_fully(G, ['A5', 'A7', 'A9', 'A10', 'A11', 'A12', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19'])
         connect_group_in_series(G, ['A4A', 'A4B'])
+        connect_group_fully(G, 
+            ['A5', 'A7', 'A9', 'A10', 'A11', 'A12', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19'])
 
         # concourse B
-        add_edge_euclidean(G, 'WH', 'B1')
+        connect_gates(G, 'WH', 'B1')
         connect_group_in_series(G, ['B1', 'B2', 'B3', 'B5'])
-        connect_group_fully(G, ['B5', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12', 'B14', 'B15', 'B16', 'B17', 'B18', 'B19',
-               'B20', 'B21', 'B22', 'B23', 'B24', 'B25', 'B26'])
+        connect_group_fully(G, 
+            ['B5', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12', 'B14', 'B15', 'B16', 'B17', 
+                'B18', 'B19', 'B20', 'B21', 'B22', 'B23', 'B24', 'B25', 'B26'])
 
         # concourse C
-        add_edge_euclidean(G, 'WH', 'C1')
+        connect_gates(G, 'WH', 'C1')
         connect_group_fully(G, ['C1', 'C2', 'C3'])
-        
+
+        assert nx.is_connected(G)
         return G
 
 
@@ -45,19 +49,19 @@ class GateGraphLoader:
         G = self.G.copy()
 
         # concourse A
-        add_edge_euclidean(G, 'WH', 'A1')
+        connect_gates(G, 'WH', 'A1')
         connect_group_in_series(G, ['WH', 'A4A', 'A4B'])
         connect_group_in_series(G, ['A1', 'A2', 'A3', 'A5'])
         connect_group_in_series(G, ['A5', 'A7', 'A9', 'A11', 'A15', 'A17'])
         connect_group_in_series(G, ['A10', 'A12', 'A14', 'A16', 'A15', 'A17'])
         connect_groups_accross(G, ['A9', 'A11', 'A14'], ['A10', 'A12', 'A15'])
         connect_group_in_series(G, ['A15', 'A17', 'A19', 'A18', 'A16', 'A14'])
-        add_edge_euclidean(G, 'A7', 'A9')
+        connect_gates(G, 'A7', 'A9')
 
         # concourse B
-        add_edge_euclidean(G, 'WH', 'B1')
+        connect_gates(G, 'WH', 'B1')
         connect_group_in_series(G, ['B1', 'B2', 'B3', 'B5'])
-        add_edge_euclidean(G, 'B5', 'B7')
+        connect_gates(G, 'B5', 'B7')
         group1 = ['B7', 'B9', 'B11', 'B15', 'B17', 'B19', 'B21']
         group2 = ['B8', 'B10', 'B12', 'B16', 'B18', 'B20', 'B22']
         connect_group_in_series(G, group1)
@@ -67,7 +71,8 @@ class GateGraphLoader:
         connect_group_in_series(G, ['B21', 'B23', 'B25', 'B26', 'B24', 'B22'])
 
         # concourse C
-        add_edge_euclidean(G, 'WH', 'C1')
+        connect_gates(G, 'WH', 'C1')
         connect_group_fully(G, ['C1', 'C2', 'C3'])
 
+        assert nx.is_connected(G)
         return G
--- src/utils.py
+++ src/utils.py
@@ -9,23 +9,23 @@ def euclidean(G, gate1, gate2):
     return np.sqrt((x1-x2)**2 + (y1-y2)**2) 
 
 
-def add_edge_euclidean(G, gate1, gate2):
-    G.add_edge(gate1, gate2, dist=euclidean(G, gate1, gate2))
+def connect_gates(G, gate1, gate2):
+    G.add_edge(gate1, gate2, weight=euclidean(G, gate1, gate2))
 
 
-def add_edges_eucliean(G, gate_pairs):
-    G.add_weighted_edges_from([
+def add_edges_euclidean(G, gate_pairs):
+    G.add_weighted_edges_from((
         (gate1, gate2, euclidean(G, gate1, gate2))
         for gate1, gate2 in gate_pairs
-    ], weight='dist')
+    ))
 
 
 def connect_group_in_series(G, group):
-    add_edges_eucliean(G, zip(group[:-1], group[1:]))
+    add_edges_euclidean(G, zip(group[:-1], group[1:]))
 
 
 def connect_group_fully(G, group):
-    add_edges_eucliean(G, ((gate1, gate2) 
+    add_edges_euclidean(G, ((gate1, gate2) 
         for gate1, gate2 in product(group, group) 
         if gate1 != gate2
     ))
@@ -33,4 +33,4 @@ def connect_group_fully(G, group):
 
 def connect_groups_accross(G, group1, group2):
     assert len(group1) == len(group2)
-    add_edges_eucliean(G, zip(group1, group2))
+    add_edges_euclidean(G, zip(group1, group2))
